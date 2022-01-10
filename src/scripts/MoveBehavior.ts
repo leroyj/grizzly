c00f76a83d46d6f42372554b91733313404c1c36
Julien <julien.leroy@djoo.org>
2022-01-11 00:29:14 +0100

    feat(core): :sparkles: add MoveByBounce and MoveRightLeft move

--- src/scripts/MoveBehavior.ts
+++ src/scripts/MoveBehavior.ts
@@ -15,11 +15,24 @@ export class MoveRightLeft implements MoveBehavior {
         if ( (positionX<500) || (positionX>775) ) {
             this.direction*=-1;
         } 
-        return [positionX-this.direction,positionY];
+        return [positionX-2*this.direction,positionY];
+    }
+}
+export class MoveByBounce implements MoveBehavior {
+    direction:number=1;
+    range=0;
+    move(positionX:number,positionY:number):[number,number] {
+        //console.log("MoveLeft.move called!");
+        if ( (this.range<0) || (this.range>11) ) {
+            this.direction*=-1;
+        }
+        this.range+=this.direction;
+        return [positionX,positionY+2*this.direction];
     }
 }
 
 export const MoveBehaviorStore: any = {
     MoveNot,
-    MoveRightLeft
+    MoveRightLeft,
+    MoveByBounce
 }
